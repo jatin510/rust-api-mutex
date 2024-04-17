64d5745b0c8508e50930edf871f6a7a4ff10d30d
jatin <jagdish@kunato.io>
2024-04-17 15:16:11 +0530

    external api trait and implementation

--- src/external/external.rs
+++ src/external/external.rs
@@ -1,18 +1,40 @@
+use async_trait::async_trait;
+use reqwest;
+
 use super::ExternalApi;
 
-struct ExternalApiService {
+pub struct ExternalApiService {
     access_token: String,
     refresh_token: String,
 }
 
+impl ExternalApiService {
+    pub fn new() -> Self {
+        ExternalApiService {
+            access_token: "hello".to_string(),
+            refresh_token: "hello".to_string(),
+        }
+    }
+}
+
+#[async_trait]
 impl ExternalApi for ExternalApiService {
-    fn get_new_access_token() -> Result<String, String> {
+    async fn get_new_access_token_api() -> Result<String, String> {
         // Implementation here
         Ok("access_token".to_string())
     }
 
-    fn get_info() -> Result<String, String> {
+    async fn get_info_api() -> Result<String, String> {
         // Implementation here
+
+        let response = reqwest::get("https://www.rust-lang.org")
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        println!("response = {response:?}");
         Ok("info".to_string())
     }
 }
--- src/external/mod.rs
+++ src/external/mod.rs
@@ -1,6 +1,9 @@
-pub trait ExternalApi {
-    fn get_new_access_token() -> Result<String, String>;
-    fn get_info() -> Result<String, String>;
-}
-
 pub mod external;
+
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait ExternalApi {
+    async fn get_new_access_token_api() -> Result<String, String>;
+    async fn get_info_api() -> Result<String, String>;
+}
