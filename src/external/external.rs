2952d1d8db64af05a7974420b01e7da01a3c9200
jatin <jagdish@kunato.io>
2024-04-17 23:23:43 +0530

    updating the access_token if the status code is 401

--- src/external/external.rs
+++ src/external/external.rs
@@ -1,22 +1,23 @@
 use async_trait::async_trait;
 use reqwest::header::{HeaderMap, AUTHORIZATION};
 use reqwest::{Client, StatusCode};
+use tokio::sync::Mutex;
 
 use super::ExternalApi;
 
 pub struct ExternalApiService {
-    access_token: String,
-    refresh_token: String,
+    access_token: Mutex<String>,
+    refresh_token: Mutex<String>,
 }
-
-static ACCESS_TOKEN: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTMzNTA3ODIsImV4cCI6MTcxMzQzNzE4Mn0.cQc4t5FCz5QOKIH0Pim5fymqnnYcLt4oUZso_jp9OM0";
-static REFRESH_TOKEN :&str= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTMzNTA0NDUsImV4cCI6MTcxMzk1NTI0NX0.p--jjgKMKS6JbSW331-aghsnQCDvLaovZmunTrz9DbA";
 
 impl ExternalApiService {
     pub fn new() -> Self {
+        let access_token: &str = "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTMzNTA3ODIsImV4cCI6MTcxMzQzNzE4Mn0.cQc4t5FCz5QOKIH0Pim5fymqnnYcLt4oUZso_jp9OM0";
+        let refresh_token :&str= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTMzNTA0NDUsImV4cCI6MTcxMzk1NTI0NX0.p--jjgKMKS6JbSW331-aghsnQCDvLaovZmunTrz9DbA";
+
         ExternalApiService {
-            access_token: ACCESS_TOKEN.to_string(),
-            refresh_token: REFRESH_TOKEN.to_string(),
+            access_token: Mutex::new(access_token.to_string()),
+            refresh_token: Mutex::new(refresh_token.to_string()),
         }
     }
 }
@@ -33,7 +34,7 @@ impl ExternalApi for ExternalApiService {
         let client = Client::new();
 
         // Create the value for the Authorization header
-        let bearer_token = self.access_token.clone();
+        let bearer_token = self.access_token.lock().await.clone();
         let auth_value = format!("Bearer {}", bearer_token);
 
         // Create a HeaderMap and insert the Authorization header
@@ -48,13 +49,66 @@ impl ExternalApi for ExternalApiService {
             .await
             .unwrap();
 
+        println!("response = {response:?}");
         println!("status: {}", response.status());
         if response.status() == StatusCode::UNAUTHORIZED {
             // spawn a new thread
-            println!("creating new thread")
+            // println!("creating new thread");
+
+            // thread::spawn(move || self.get_new_access_token_api());
+            drop(bearer_token); // Release the lock before refreshing token
+            self.refresh_access_token().await?;
+            return self.get_info_api().await; // Retry the request
         }
 
-        println!("response = {response:?}");
         Ok("info".to_string())
+    }
+
+    async fn refresh_access_token(&self) -> Result<String, String> {
+        let client = reqwest::Client::builder().build().unwrap();
+
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert("Content-Type", "application/json".parse().unwrap());
+
+        let refresh_token = self.refresh_token.lock().await.clone();
+
+        let data = format!(
+            r#"{{
+            "token": "{}"
+        }}"#,
+            refresh_token
+        );
+
+        let json: serde_json::Value = serde_json::from_str(&data).unwrap();
+
+        let request = client
+            .request(
+                reqwest::Method::POST,
+                "http://localhost:3000/refresh-access-token",
+            )
+            .headers(headers)
+            // .body(&json);
+            .json(&json);
+
+        let response = request.send().await.unwrap();
+        if response.status() == StatusCode::OK {
+            let data: serde_json::Value = response.json().await.unwrap();
+
+            let new_access_token = data["accessToken"].as_str().unwrap().to_string();
+            let new_refresh_token = data["refreshToken"].as_str().unwrap().to_string();
+
+            self.update_tokens(new_access_token, new_refresh_token)
+                .await;
+        }
+
+        Ok("access_token".to_string())
+    }
+
+    async fn update_tokens(&self, new_access_token: String, new_refresh_token: String) {
+        let mut access_token_guard = self.access_token.lock().await;
+        let mut refresh_token_guard = self.refresh_token.lock().await;
+
+        *access_token_guard = new_access_token;
+        *refresh_token_guard = new_refresh_token;
     }
 }
--- src/external/mod.rs
+++ src/external/mod.rs
@@ -6,4 +6,6 @@ use async_trait::async_trait;
 pub trait ExternalApi {
     async fn get_new_access_token_api(&self) -> Result<String, String>;
     async fn get_info_api(&self) -> Result<String, String>;
+    async fn refresh_access_token(&self) -> Result<String, String>;
+    async fn update_tokens(&self, new_access_token: String, new_refresh_token: String);
 }
